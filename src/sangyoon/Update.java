6497e7572e82b3e20455021e4bbaf1cf00d01fce
thsdbqls <thsdbqls3672@gmail.com>
2026-02-04 09:32:40 +0900

    v7.0

--- src/sangyoon/Update.java
+++ src/sangyoon/Update.java
@@ -7,7 +7,7 @@ import d260202_03.AccountBook;
 
 public interface Update {
 	//public Phonebook findByIndate(String indate);
-	public int SelectUpdate(String category, String indate);
+	//public int SelectUpdate(String category, String indate);
 	public int SelectUpdate2(AccountBook ab);
 	
 	
